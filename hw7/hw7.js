3fc8a2ef0e8cd963ca6802e7306484aebee5c552
vadzim84 <vkaraniec@gmail.com>
2020-02-06 16:15:18 +0300

    fix(hw7)
    Тask 1: добавлена функция renderResponce.
    Task 2: исправлен метод Promice;
    вынести let value = limit.value за пределы метода Promice не ударось, т.к. при старте кода "value" инициализируется как пустая строка, после чуго данные из "input" не получаетс передать в функцию "this.start"

--- hw7/hw7.js
+++ hw7/hw7.js
@@ -1,15 +1,19 @@
 // // Task 1
 
-fetch('https://cors-anywhere.herokuapp.com/' + 'https://api.darksky.net/forecast/782df41cc8f086cc823109fb02e0cfbd/53.6807701,23.831889?exclude=currently,flags,hourly&lang=be&units=auto')
-  .then(response => response.json())
-    .then(result => console.log(`Надвор'е на заўтра ў Гораднi:
-      +${Math.round(result['daily']['data']['1']['temperatureLow'])} +${Math.round(result['daily']['data']['1']['temperatureHigh'])},
-      вiльготнасць ${result['daily']['data']['1']['humidity'] * 100}%,
-      хуткасць ветра ${Math.round(result['daily']['data']['1']['windSpeed'])}м/с з парывамi да ${Math.round(result['daily']['data']['1']['windGust'])}м/с,
-      бачнасць ${Math.round(result['daily']['data']['1']['visibility'])}км,
-      цiск ${result['daily']['data']['1']['pressure']} мм рт. сл.
-      ${result['daily']['data']['1']['summary']}`))
+// fetch('https://cors-anywhere.herokuapp.com/' + 'https://api.darksky.net/forecast/782df41cc8f086cc823109fb02e0cfbd/53.6807701,23.831889?exclude=currently,flags,hourly&lang=be&units=auto')
+//     .then(response => response.json())
+//     .then(result => renderResponce(result));
 
+// function renderResponce(result) {
+//     const data = result['daily']['data']['1'];
+//     console.log(`Надвор'е на заўтра ў Гораднi:
+//       ${Math.round(data['temperatureLow'])} ${Math.round(data['temperatureHigh'])},
+//       вiльготнасць ${data['humidity'] * 100}%,
+//       хуткасць ветра ${Math.round(data['windSpeed'])}м/с з парывамi да ${Math.round(data['windGust'])}м/с,
+//       бачнасць ${Math.round(data['visibility'])}км,
+//       цiск ${Math.round(data['pressure'])} мм рт. сл.
+//       ${data['summary']}`)
+// }
 
 // Task 2
 
@@ -38,7 +42,8 @@ function Timer(limit) {
         clearInterval(intervalId);
         let timeLimit = number1.value * 1000;
         setTimeout(() => {
-            this.start();
+            this.start()
+                .then(resolve => console.log(resolve));
         }, timeLimit);
     };
 }
@@ -55,28 +60,12 @@ btnStart.addEventListener('click', () => {
         alert('Enter a pause no more than 60 seconds!')
         return false;
     }
-    promiseStart();
+    timer.start()
 });
 
 btnPause.addEventListener('click', (event) => {
     event.preventDefault();
     timer.pause();
-    promiseStart2();
-});
-
-
-promiseStart2 = function () {
     timer2.start()
-        .then((value) => {
-            console.log(value);
-        })
-        .catch(error => console.log(error.message))
-}
-
-promiseStart = function () {
-    timer.start()
-        .then((value) => {
-            console.log(value);
-        })
-        .catch(error => console.log(error.message))
-}
+        .then(resolve => console.log(resolve))
+});
